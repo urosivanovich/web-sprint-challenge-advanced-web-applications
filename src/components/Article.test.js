853791dcec292b48260def8446db0cb1abeb9cbe
Uros Ivanovic <urosivanovic@Uross-MBP.lan>
2022-01-28 14:13:11 -0500

    mvp done

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { findAllByTestId, render,  screen, waitFor } from '@testing-library/react';
+import { render,  screen } from '@testing-library/react';
 
 import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
 
-const mockProps = {
+const mockArt = {
     id: 'aMqwd',
     headline: "headline",
     createdOn: '2021-08-09T18:02:38-04:00',
@@ -18,14 +18,13 @@ const mockProps = {
 
 
 test('renders component without errors', () => {
-    render(<Article article={mockProps} />)
-    console.log(<Article />)
+    render(<Article article={mockArt} />)
 });
 
-test('renders headline, author from the article when passed in through props', async () => {
-    render(<Article article={mockProps} />)
-    const headline = await screen.findByText('headline')
-    const author =  await screen.findByTestId('author')
+test('renders headline, author from the article when passed in through props', () => {
+    render(<Article article={mockArt} />)
+    const headline =  screen.queryByText('headline')
+    const author =  screen.queryByTestId('author')
     expect(headline).toBeInTheDocument()
     expect(headline).toBeVisible()
     expect(headline).toBeTruthy()
@@ -34,17 +33,18 @@ test('renders headline, author from the article when passed in through props', a
     expect(author).toBeTruthy()
 });
 
-test('renders "Associated Press" when no author is given', async () => {
-    mockProps.author = ''
-    render(<Article article={mockProps} />)
-    const author = await screen.findByText(/associated press/i)
+test('renders "Associated Press" when no author is given', () => {
+    mockArt.author = ''
+    render(<Article article={mockArt} />)
+    const author =  screen.queryByText(/associated press/i)
     expect(author).toBeInTheDocument()
+    expect(author).toHaveTextContent(/associated press/i)
 });
 
 test('executes handleDelete when the delete button is pressed', () => {
     const handleDelete = jest.fn()
 
-    render(<Article article={mockProps} handleDelete={handleDelete} />)
+    render(<Article article={mockArt} handleDelete={handleDelete} />)
     const button = screen.getByTestId('deleteButton')
     userEvent.click(button)
     expect(handleDelete).toHaveBeenCalled()
